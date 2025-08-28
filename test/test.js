3f437017b4018f079649a761c53c79087d12e7a9
shyguyrymakesai <ryan.martinez2@protonmail.com>
2025-08-28 15:23:41 +0000

    yay

--- test/test.js
+++ test/test.js
@@ -12,3 +12,10 @@ describe('First two terms', function() {
       assert.strictEqual(main.fibonacci(1), 1);
     });
 });
+
+describe('Negative value', function() {
+  it('should return undefined', function() {
+      assert.strictEqual(main.fibonacci(-5), undefined);
+      assert.strictEqual(main.fibonacci(-1), undefined);
+    });
+});
